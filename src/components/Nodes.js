8f34dc2ca1b0557ddbbf85059fea3314e8f2cded
yeon <dalcon10280@gmail.com>
2021-04-30 01:13:59 +0900

    Event Delegation 적용

--- src/components/Nodes.js
+++ src/components/Nodes.js
@@ -8,6 +8,7 @@ export default class Nodes {
     this.onBackClick = onBackClick
 
     this.render()
+    this.setEvent()
   }
 
   setState (nextState) {
@@ -16,8 +17,6 @@ export default class Nodes {
   }
 
   render () {
-    // this.$target.innerHTML = this.state.nodes.map(node => `<li>${node.name}</li>`)
-
     if (this.state.nodes) {
       const nodesTemplate = this.state.nodes.map(node => {
         const iconPath = node.type === 'FILE' ? './assets/file.png' : './assets/directory.png'
@@ -34,21 +33,27 @@ export default class Nodes {
       // 뒤로가기의 경우 data-node-id attribute를 렌더링하지 않음
       this.$target.innerHTML = !this.state.isRoot ? `<div class="Node"><img src="./assets/prev.png"></div>${nodesTemplate}` : nodesTemplate
     }
+  }
 
-    this.$target.querySelectorAll('.Node').forEach($node => {
-      $node.addEventListener('click', (e) => {
-        // dataset을 통해 data-로 시작하는 attribute를 꺼내올 수 있음
-        const { nodeId } = e.target.dataset
-        // nodeId가 없는 경우 뒤로가기를 누른 케이스
+  setEvent () {
+    this.$target.addEventListener('click', (e) => {
+      // $target 하위에 있는 HTML 요소 클릭시 이벤트가 상위로 계속 전파 되면서
+      // $target까지 오게 됨. 이 특성을 이용한 기법.
+      // closest를 이용하면 현재 클릭한 요소와 제일 인접한 요소를 가져올 수 있음.
+      const $node = e.target.closest('.Node')
+      if ($node) {
+        const { nodeId } = $node.dataset
         if (!nodeId) {
           this.onBackClick()
+          return
         }
+
         const selectedNode = this.state.nodes.find(node => node.id === nodeId)
-        console.log(selectedNode)
+
         if (selectedNode) {
           this.onClick(selectedNode)
         }
-      })
+      }
     })
   }
 }
